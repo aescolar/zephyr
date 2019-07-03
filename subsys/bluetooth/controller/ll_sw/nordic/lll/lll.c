b10df9611cfcb10b7145383796dee14c41f1eb36
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2019-07-03 09:19:51 +0200

    bluetooth: controller: Align nrf link layer to new clock control API
    Align to use clock_control_get_status for blocking clock start.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
@@ -127,7 +127,7 @@ int lll_init(void)
 		return -ENODEV;
 	}
 
-	clock_control_on(clk_k32, (void *)CLOCK_CONTROL_NRF_K32SRC);
+	clock_control_on(clk_k32, NULL);
 
 	/* Initialize HF CLK */
 	lll.clk_hf =
@@ -318,9 +318,12 @@ int lll_clk_on_wait(void)
 	int err;
 
 	/* turn on radio clock in blocking mode. */
-	err = clock_control_on(lll.clk_hf, (void *)1);
-	if (!err || err == -EINPROGRESS) {
+	err = clock_control_on(lll.clk_hf, NULL);
+
+	while (clock_control_get_status(lll.clk_hf, NULL) !=
+			CLOCK_CONTROL_STATUS_ON) {
 		DEBUG_RADIO_XTAL(1);
+		k_cpu_idle();
 	}
 
 	return err;
