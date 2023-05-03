4504e15de5f832f219b9c42e9de2668fdb612d86
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-05-03 12:42:49 +0200

    a bit more

--- boards/posix/linux_runner_sc/runner/finnish_build.sh
+++ boards/posix/linux_runner_sc/runner/finnish_build.sh
@@ -7,9 +7,9 @@ C_FLAGS="-g $COVERAGE_COMP $ARCH -fno-pie -fno-pic -no-pie"
 C_FLAGS_RELINK="$C_FLAGS -Wl,--gc-sections"
 
 #objcopy --localize-hidden  zephyr/zephyr.elf zephyr/zephyr.post.elf -w --localize-symbols=${ZEPHYR_BASE}/boards/posix/linux_runner_sc/linker_symbols_to_localize
-objcopy --localize-hidden zephyr/zephyr.elf cpu_0.sw.o -w --localize-symbol=_*
+objcopy --localize-hidden zephyr.elf cpu_0.sw.o -w --localize-symbol=_*
 
-gcc $C_FLAGS -c ${ZEPHYR_BASE}/boards/posix/linux_runner_sc/main.c -o runner.o -I../include -I${ZEPHYR_BASE}/soc/posix/inf_clock/ -I${ZEPHYR_BASE}/boards/posix/linux_runner_sc/ -include zephyr/include/generated/autoconf.h
+gcc $C_FLAGS -c ${ZEPHYR_BASE}/boards/posix/linux_runner_sc/runner/main.c -o runner.o -I${ZEPHYR_BASE}/include -I${ZEPHYR_BASE}/soc/posix/inf_clock/ -I${ZEPHYR_BASE}/boards/posix/linux_runner_sc/ -include include/generated/autoconf.h
 
 #echo "#Final:"
-gcc runner.o cpu_0.sw.o -o zephyr.exe $C_FLAGS_RELINK
+gcc runner.o cpu_0.sw.o -o linux_runner.exe $C_FLAGS_RELINK
