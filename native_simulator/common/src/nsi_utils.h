2dd22e6f7158bf7be7c5cd3184c6c547b7419509
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-05-26 18:04:42 +0200

    Native simulator: Add first version in-tree
    Add the first version of the native simulator
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ native_simulator/common/src/nsi_utils.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2010-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef NSI_COMMON_SRC_INCL_NSI_UTILS_H
+#define NSI_COMMON_SRC_INCL_NSI_UTILS_H
+
+
+#define _NSI_STRINGIFY(x) #x
+#define NSI_STRINGIFY(s) _NSI_STRINGIFY(s)
+
+/* concatenate the values of the arguments into one */
+#define NSI_DO_CONCAT(x, y) x ## y
+#define NSI_CONCAT(x, y) NSI_DO_CONCAT(x, y)
+
+#define NSI_MAX(a, b)  (((a) > (b)) ? (a) : (b))
+#define NSI_MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+#ifndef NSI_ARG_UNUSED
+#define NSI_ARG_UNUSED(x) (void)(x)
+#endif
+
+#endif /* NSI_COMMON_SRC_INCL_NSI_UTILS_H */
