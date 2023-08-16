46e164d110ec8c50ddcc479e87e843b9f140d73e
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-08-16 15:19:03 +0200

    nicer

--- tests/bsim/build_parallel.sh
+++ tests/bsim/build_parallel.sh
@@ -18,33 +18,46 @@ err=0
 i=0
 
 if [ -n "${TESTS_FILE}" ]; then
-	#remove comments and empty lines from file
-	all_cases=$(sed 's/#.*$//;/^$/d' "${TESTS_FILE}")
+  #remove comments and empty lines from file
+  all_cases=$(sed 's/#.*$//;/^$/d' "${TESTS_FILE}")
 elif [ -n "${TESTS_LIST}" ]; then
-	all_cases=${TESTS_LIST}
+  all_cases=${TESTS_LIST}
 else
-	SEARCH_PATH="${SEARCH_PATH:-.}"
-	all_cases=`find ${SEARCH_PATH} -name "*.sh" | \
-	         grep -Ev "(/_|run_parallel|build_parallel.sh|compile.*sh|generate_coverage_report.sh)"`
-	#we dont run ourselves
+  SEARCH_PATH="${SEARCH_PATH:-.}"
+  all_cases=`find ${SEARCH_PATH} -name "*.sh" | \
+           grep -Ev "(/_|run_parallel|build_parallel.sh|compile.*sh|generate_coverage_report.sh)"`
+  #we dont run ourselves
 fi
 
 set -u
 
 all_cases_a=( $all_cases )
-n_cases=$((${#all_cases_a[@]}))
 
 needed_apps=()
-
 #find all lines in the testcase scripts starting with '#.*Requires:'
-#remove posible trailing comments, and trim spaces 
-needed_apps+=$(grep -E "#.*Requires:" $all_cases | sed 's/.*#.*Requires://g' | sed 's/#.*$//;/^$/d' | xargs)
- 
+#remove posible trailing comments, and trim spaces
+needed_apps+=$(grep -E "#.*Requires:" $all_cases | \
+               sed 's/.*#.*Requires://g' | \
+               sed 's/#.*$//;/^$/d' | xargs)
 # Remove duplicates
 needed_apps_sorted=$(echo $needed_apps | sort | uniq)
-#echo $needed_apps_sorted
 
-build_scripts=$(echo $needed_apps_sorted | awk 'NR > 1 {printf("-o ")}; {print "-name compile."$0".sh"}' | xargs find ${ZEPHYR_BASE}/tests/bsim/)
+missing_apps=$(grep -L -E "#.*Requires:" $all_cases)
+if [ -n "${missing_apps}" ]; then
+  all_missing_a=( $missing_apps )
+  echo -e "\e[91mWarning, the following $((${#all_missing_a[@]}))"\
+          "test scripts do not specify any required test apps:\e[39m"
+#  printf '%s\n' "${all_missing_a[@]}"
+  echo -e "\e[91mContinuing with the apps from tests that do\e[39m"
+fi
+
+build_scripts=$(echo $needed_apps_sorted | \
+                awk 'NR > 1 {printf("-o ")}; {print "-name compile."$0".sh"}' | \
+                xargs find ${ZEPHYR_BASE}/tests/bsim/ )
+
+build_scripts_a=( $build_scripts )
+n_apps=$((${#build_scripts_a[@]}))
+echo "Attempting to build ${n_apps} app"
 
 if [ `command -v parallel` ]; then
   parallel '
